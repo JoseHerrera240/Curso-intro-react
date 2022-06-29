d486c341f060fe96c50b15ca02a9de29c798abb6
Jose Herrera <hjose0650gmail.com>
2022-06-28 22:13:51 -0500

    Realizamos la separaciòn de carpetas

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch";
-import { TodoList } from "./TodoList";
-import { TodoItem } from "./TodoItem";
-import { CreateTodoButton } from "./CreateTodoButton";
+import { AppUI } from "./AppUI";
 // import './App.css';
 
 const defaultTodos = [
@@ -30,24 +26,29 @@ function App() {
       return todoText.includes(searchText);
     });
   }
-  
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
   return (
-   <React.Fragment>
-      <TodoCounter 
-        total={totalTodos}
-        completed={completedTodos}
-      />    
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      <TodoList>
-        {searchedTodos.map(todo => (
-            <TodoItem key={todo.text}  text={todo.text} completed={todo.completed}/>
-        ))}
-      </TodoList>
-      <CreateTodoButton />      
-   </React.Fragment>
+    <AppUI 
+      total={totalTodos}
+      completed={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
